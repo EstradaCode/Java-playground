54c3a8b60f3ae51124bffc34edb7a0706e2125c6
Francisco <franciscoestrada565@gmail.com>
2024-03-17 15:20:08 -0300

    se añadió la materia prograIII y testing de java collections

---
+++ Collections/src/Lists/ArrayTest.java
@@ -0,0 +1,8 @@
+package Lists;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ArrayTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ PrograIII/practica1/src/tp1/ej2/ejercicio2.java
@@ -0,0 +1,20 @@
+package tp1.ej2;
+import java.util.Scanner;
+public class ejercicio2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int[]array= tamanioArreglo(in.nextInt());
+		for( int numero : array) {
+			System.out.print(numero + " - ");
+		}
+	}
+	public static int[] tamanioArreglo(int n) {
+		int [] array= new int[n];
+		for( int i = 0 ; i< n ; i++) {
+			array[i] = n*(i+1);
+		}
+		return array;
+	}
+
+}
